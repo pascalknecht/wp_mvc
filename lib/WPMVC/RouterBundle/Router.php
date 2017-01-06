1005f86ff4ab4e405e4e4bfbe5f9a8359eb02b11
pascalknecht <pascal.knecht@cubetech.ch>
2017-01-06 13:52:23 +0100

    Merging

--- lib/WPMVC/RouterBundle/Router.php
+++ lib/WPMVC/RouterBundle/Router.php
@@ -10,13 +10,16 @@ class Router {
         }
         // Include Controller
         include_once( $controller_file_name );
-
-        add_action( 'wp_router_generate_routes', function( $router ) use( $route_name, $route, $controller, $method ) {
+	    
+        add_action( 'wp_router_generate_routes', function( $router ) use(  $route_name, $route, $controller, $method ) {
             $controller_class_name = '\\' . $controller;
             $router->add_route(
-	            $route_name, array(
+                $route_name, array(
                     'path' => $route,
-                    'query_vars' => array(),
+                    'query_vars' => array(
+                        'company_name' => 1,
+                    ),
+                    'page_arguments' => array('company_name'),
                     'page_callback' => array( new $controller_class_name, $method ),
                     'access_callback' => true,
                     'title' => 'Test Title',
